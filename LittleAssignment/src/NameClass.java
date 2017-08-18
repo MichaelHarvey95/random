ebba66631b1c0fbcbcd863ff99bed3d37360b801
MHarvey <L00102447@student.lyit.ie>
2017-08-18 19:14:51 +0100

    My first commit

---
+++ LittleAssignment/src/NameClass.java
@@ -0,0 +1,11 @@
+
+public class NameClass {
+	
+	public static void main(String [] args)
+	{
+		String firstName = "Michael ";
+		String lastName = "Devine";
+		
+		System.out.println(firstName.concat(lastName));
+	}
+}
